e27689d525183f8fb497a12ba63c09963da906e9
kracak <scislokuba@gmail.com>
2023-10-30 10:16:14 +0100

    Dzialajacy localhost

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,8 @@
 const mysql = require('mysql')
 const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000;
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -16,3 +19,5 @@ connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
 })
 
 connection.end()
+
+app.listen(PORT , () => console.log("Working on port "+PORT))
