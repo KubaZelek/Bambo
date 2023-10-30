d68cc3a4387186251019c749d87bb74b068980af
Maciej Szabat/mtroxeq <matroxszabat@gmail.com>
2023-10-30 11:07:49 +0100

    Nic nie zmieniono

--- Backend/server.js
+++ Backend/server.js
@@ -15,10 +15,9 @@ connection.connect()
 connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
   if (err) throw err
 
-  console.log('The solution is: ', rows[0].solution)
+  console.log('Database is working')
 })
 
 connection.end()
 
 app.listen(PORT , () => console.log("Working on port "+PORT))
-
