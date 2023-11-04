2b709f61d9a9da7492b6520a7ab89dd386b4ba35
mtroxeq <matroxszabat@gmail.com>
2023-11-04 11:23:09 +0100

    Dodano rejestrację

--- Backend/server.js
+++ Backend/server.js
@@ -14,10 +14,23 @@ connection.connect()
 
 connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
   if (err) throw err
-
   console.log('Database is working')
 })
 
-connection.end()
 
 app.listen(PORT , () => console.log("Working on port "+PORT))
+
+app.post('/signup', (req, res) => {
+  const sql = "INSERT INTO login ('username','password','email') values (?)";
+  const values = [
+    req.body.username,
+    req.body.email,
+    req.body.password
+  ]
+  connection.query(sql, [values], (err, data) => {
+    if(err) return res.json(err);
+    return res.json(data);
+  })
+})
+
+connection.end()
