ad790103d97f956554c7bc2b410974d6f6c1d78b
Maciej Szabat/mtroxeq <matroxszabat@gmail.com>
2023-11-06 10:40:44 +0100

    +Auction

---
+++ BamboPL/src/Auctions/create-auction.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Signup() {
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+    email: '',
+  });
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post('http://localhost:5000/create_auction', values)
+      .then((res) => {
+        console.log("Registered!");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="Signup">
+      <h1>Rejestracja</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Nazwa użytkownika</label>
+        <input type="text" name="username" placeholder="Wpisz nazwę" onChange={handleChange} />
+        <label htmlFor="password">Hasło</label>
+        <input type="password" name="password" placeholder="Wpisz hasło" onChange={handleChange} />
+        <label htmlFor="email">E-mail</label>
+        <input type="text" name="email" placeholder="Wpisz e-mail" onChange={handleChange} />
+        <button type="submit">Zarejestruj</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
